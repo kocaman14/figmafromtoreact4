f44326c19257d9d6484cdae0312b5229e17dd06a
kocaman14 <o.kcmn@hotmail.com>
2024-12-30 21:42:34 +0300

    figmatoreact4

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+
+function App() {
+
+
+  return (
+    
+      <div className="w-full h-screen flex  overflow-hidden justify-between  items-center bg-[rgba(2,0,79,0.93)]">
+       <div>
+<img src="./images/Polygon 1.png" className=" w-[700.5px]  h-[655px]  sm:w-[600.5px]   sm:h-[850px] "/>
+       </div>
+<div className="   h-[361px] w-[512px] sm:h-[300px] sm:w-[512px] flex flex-col items-start ">
+<h1 className="sm:w-[286px] sm:h-[85px]  gap-0 font-montserrat text-[70px] font-bold leading-[85.33px] text-left  decoration-skip-ink-none text-[rgba(255,255,255,0.5)]">LOGIN</h1>
+<div>
+<input placeholder="Username" className="sm:w-[469.31px] sm:h-[60px] mt-4 text-xl gap-0 border border-[rgba(255,255,255,0.1)] bg-[rgba(255,255,255,0.01)]"></input>
+</div>
+<div>
+<input placeholder="Password" className="sm:w-[469.31px] sm:h-[60px] mt-4 text-xl gap-0 border border-[rgba(255,255,255,0.1)] bg-[rgba(255,255,255,0.01)]"></input>
+</div>
+<div className="mt-4 text-center">
+ <button className="w-[250px] h-[60px] text-xl text-white   font-bold   font-montserrat border border-[rgba(255,255,255,0.5)] bg-[rgba(2,0,79,0.93)]">Login</button> 
+</div>
+</div>
+
+     
+<div>
+<img src="./images/Polygon 1.png" className="w-[700.5px]  h-[655px]  sm:h-[850px]    lg:mt-5 xl:mt-20  rotate-[-180deg]"/>
+
+</div>
+
+
+
+
+
+
+      </div>
+    
+  )
+}
+
+export default App
